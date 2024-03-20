ff529097b3660427090873612702004cebc9d6bf
Ayan Basu <92411611+AyanB15@users.noreply.github.com>
2024-03-20 13:38:48 -0400

    Add files via upload

---
+++ src/config.py
@@ -0,0 +1,9 @@
+
+TRAINING_FILE_TEST = '../data/flight_test.csv'
+TRAINING_FILE_HISTORY = '../data/flight_history.csv'
+
+MODELS = '../models'
+
+
+MODEL_TRAINING_FILE='../data/training_data_processed.csv'
+
